72c91a7509828105e308f72e9ac4e6dd04e98a35
Mike Pennisi <mike@mikepennisi.com>
2018-10-17 20:16:50 -0400

    Add Tornado implementation

---
+++ src/using_tornado.py
@@ -0,0 +1,58 @@
+import json
+import os
+import shutil
+import subprocess
+import tempfile
+import time
+
+from tornado import websocket
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado import gen
+
+def get_json(url):
+    # TODO: Investigate why urllib rejects the response from Chrome's HTTP
+    # server
+    #response = urllib.urlopen(url)
+    #return json.loads(response.read())
+
+    return json.loads(subprocess.check_output(['curl', '--silent', url]))
+
+port = 9876
+profile_dir = tempfile.mkdtemp()
+chrome = subprocess.Popen(
+    ['chromium-browser', '--user-data-dir=%s' % profile_dir, '--remote-debugging-port=%s' % port, 'http://example.com/5543'],
+    stderr=open(os.devnull, 'w')
+)
+
+@gen.coroutine
+def main():
+    try:
+        while True:
+            try:
+                instances = get_json('http://localhost:%s/json' % port)
+                break
+            except:
+                pass
+
+        for instance in instances:
+            if instance['url'] == 'http://example.com/5543':
+                debugger_url = instance['webSocketDebuggerUrl']
+                break
+        else:
+            raise Exception('Could not find browser')
+
+        connection = yield websocket.websocket_connect(debugger_url)
+        yield connection.write_message(
+            '{"id":122,"method":"Page.navigate","params":{"url":"http://bocoup.com"}}'
+        )
+
+        print (yield connection.read_message())
+
+        time.sleep(5)
+
+    finally:
+        chrome.terminate()
+        shutil.rmtree(profile_dir)
+
+if __name__ == '__main__':
+    IOLoop.current().run_sync(main)
